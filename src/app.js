de2be0c12c0db1069cbe6e26e066051556782703
NielsvanLier <niels_van_lier@hotmail.com>
2022-04-12 16:40:17 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 console.log('Hallo daar!');
+
+import axios from 'axios';
+
+async function fetchJoke() {
+    try {
+        const result = await axios.get('https://api.chucknorris.io/jokes/random');
+        console.log(result);
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+fetchJoke();
