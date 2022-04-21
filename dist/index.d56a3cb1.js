d2e848b1c16e10d915d2a35999249d5c7245223b
NielsvanLier <niels_van_lier@hotmail.com>
2022-04-21 18:01:27 +0200

    opdracht 1

--- dist/index.d56a3cb1.js
+++ dist/index.d56a3cb1.js
@@ -462,16 +462,63 @@ function hmrAcceptRun(bundle, id) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _axios = require("axios");
 var _axiosDefault = parcelHelpers.interopDefault(_axios);
-console.log('Hallo daar!');
-async function fetchJoke() {
+async function getFromApi() {
     try {
-        const result = await _axiosDefault.default.get('https://api.chucknorris.io/jokes/random');
-        console.log(result);
+        const result = await _axiosDefault.default.get('https://restcountries.com/v2/all');
+        console.log(result.data[0]);
+        result.data.sort((a, b)=>{
+            return a.population - b.population;
+        });
+        result.data.map((i)=>{
+            let newElement = document.createElement('div');
+            newElement.classList.add('country-card');
+            newElement.innerHTML = `<li class="country-data"><p class="country-name">${i.name}</p>
+                               <p class="country-population">Has a population of ${i.population} people</p>
+                               <img class="flag" src="${i.flag}"></></li>`;
+            const landList = document.getElementById('landen');
+            landList.appendChild(newElement);
+            const region = i.region;
+            return region;
+        });
+        const region = i.region;
+        let regionColor = '';
+        //regiokleur teruggeven
+        if (region === 'Africa') regionColor = 'blue';
+        else if (region === 'Americas') regionColor = 'green';
+        else if (region === 'Asia') regionColor = 'red';
+        else if (region === 'Europe') regionColor = 'yellow';
+        else if (region === 'Oceania') regionColor = 'purple';
+        else regionColor = 'black';
+        //style de namen van de landen op regiokleur
+        document.getElementsByClassName('country-name').style.color = 'pink';
+        return result; //Kan dit niet lezen buiten de functie, why??
     } catch (e) {
         console.error(e);
     }
 }
-fetchJoke();
+getFromApi(); /*
+const region = "Africa";
+ switch (region) {
+     case 'Africa':
+         return 'blue';
+         break;
+     case  'Americas':
+         return 'green';
+         break;
+     case 'Asia':
+         return 'red';
+         break;
+     case  'Europe':
+         return 'yellow';
+         break;
+     case 'Oceania':
+         return 'purple';
+         break;
+     default:
+         return 'black';
+         break;
+ }
+*/ 
 
 },{"axios":"1IeuP","@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}],"1IeuP":[function(require,module,exports) {
 module.exports = require('./lib/axios');
